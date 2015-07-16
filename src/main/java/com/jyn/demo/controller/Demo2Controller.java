b87306c19eb2e336bd79e4ee0a5839996acbacdd
niannian199 <niannian199@sina.com>
2015-07-16 15:05:24 +0800

    'modify'

--- src/main/java/com/jyn/demo/controller/Demo2Controller.java
+++ src/main/java/com/jyn/demo/controller/Demo2Controller.java
@@ -15,7 +15,7 @@ public class Demo2Controller {
 	
 	@RequestMapping(value="")
 	public ModelAndView index(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView mav = new ModelAndView("/index");
+		ModelAndView mav = new ModelAndView("/index2");
 		return mav;
 	}
 }
